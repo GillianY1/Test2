18250ba8cf57b6758fcaf8cab5f6561d35c69a94
GillianY1 <gilliangilliany@gmail.com>
2022-09-30 17:42:32 +0800

    property binding > generate pwd

--- pwdGenerator/src/app/app.component.ts
+++ pwdGenerator/src/app/app.component.ts
@@ -7,16 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  length = 0;
   includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onChangeLength(event: Event){
+    const parseLength = parseInt((event.target as HTMLInputElement).value);
+    if(!isNaN(parseLength))
+    {
+      this.length = parseLength;
+    }
+  }
 
   onChangeUseLetters(){
     this.includeLetters = !this.includeLetters;
   }
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
 
   onButtonClick(){
-    this.password ='hello kitty'
-    console.log('Button was clicked !')
-    console.log(`include letters: ${this.includeLetters}`)
+    const Numbers = '1234567890'
+    const Symbols = '!@#$%^&*()_+'
+    const Letters ='qwertyuiopasdfghjklzxcvbnm';
+
+    let validCharts ='';
+    let generatedPwd ='';
+
+    if(this.includeLetters) {validCharts += Letters;}
+    if(this.includeNumbers) {validCharts += Numbers;}
+    if(this.includeSymbols) {validCharts += Symbols;}
+
+    for (let i=0 ; i<this.length; i++){
+      const chartIndex = Math.floor(Math.random() *validCharts.length)
+      generatedPwd += validCharts[chartIndex]
+    }
+    this.password =  generatedPwd;
+
+   /* console.log(`
+    include letters: ${this.includeLetters}
+    include numbers: ${this.includeNumbers}
+    include symbols: ${this.includeSymbols}
+    lengh : ${this.length}
+    `);
+    */
   }
 
 
