85785689e15edba5b78220a2096eed59ee7a1c46
GillianY1 <gilliangilliany@gmail.com>
2022-10-04 15:47:58 +0800

    2. generate card component

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'cards';
+  //title = 'cards';
+  // ? why cannot set let or const
+  posts =[
+    {
+      title:'Neat Tree',
+      imageUrl:'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw aneat tree today'
+    },
+    {
+      title:' Snowy mountain',
+      imageUrl:'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'lets go to the moutain'
+    },
+    {
+      title:' mountaion bicking',
+      imageUrl:'assets/bicking.jpeg',
+      username: 'bicking man',
+      content: 'let us go bicking'
+    }
+  ];
 }
