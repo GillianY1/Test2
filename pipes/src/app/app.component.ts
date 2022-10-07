f469dfc0860f343b408d287ce6daa6b1b9579d9b
GillianY1 <gilliangilliany@gmail.com>
2022-10-07 18:01:16 +0800

    add pipes: date, decimal , currency

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -7,9 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  name: string | undefined ;
+ payDate: string | undefined;
+ amount: number | undefined;
+ height: number | undefined;
 
  onNameChange(event: Event){
   this.name = (event.target as HTMLInputElement).value;
+ }
+ onDateChange(event: Event){
+  this.payDate = (event.target as HTMLInputElement).value;
+ }
+ onAmountChange(event: Event){
+  this.amount = parseInt((event.target as HTMLInputElement).value);
+ }
+ onHeightChange(event: Event){
+  this.height = parseFloat((event.target as HTMLInputElement).value);
  }
 
 }
