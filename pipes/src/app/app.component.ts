ed07b5175dad78446068d429d4d824f22fc27387
GillianY1 <gilliangilliany@gmail.com>
2022-10-07 16:18:56 +0800

    basic project from pipes practice

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ name: string | undefined ;
+
+ onNameChange(event: Event){
+  this.name = (event.target as HTMLInputElement).value;
+ }
+
+}
