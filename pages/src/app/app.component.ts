fe2741d5ffabd4551b43e268c8aa1ff7698e610d
GillianY1 <gilliangilliany@gmail.com>
2022-10-11 16:54:59 +0800

    setup for directives

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {title: 'cinque terre',
+    url:'https://unsplash.com/photos/Y385ogps3-E'
+    },
+    {title: 'lago maggiore',
+    url:'https://unsplash.com/photos/fa8eqoDQT4w'
+    },
+    {title: 'venice',
+    url:'https://unsplash.com/photos/RKsLQoSnuTc'
+    },
+    {title: 'firenze',
+    url:'https://unsplash.com/photos/Qq1fwSLM0N0'
+    }
+  ]
 }
