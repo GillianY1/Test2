fb1175ebab66b40cf9331f086fae3474cddf1b63
GillianY1 <gilliangilliany@gmail.com>
2022-10-11 17:58:06 +0800

    ngClass directives, in pagination example

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -6,18 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentPage = 0;
   images = [
     {title: 'cinque terre',
-    url:'https://unsplash.com/photos/Y385ogps3-E'
+    url:'https://images.unsplash.com/photo-1616234643798-12dc87da67b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80'
     },
     {title: 'lago maggiore',
-    url:'https://unsplash.com/photos/fa8eqoDQT4w'
+    url:'https://images.unsplash.com/photo-1628704438310-d0d6424b001c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {title: 'venice',
-    url:'https://unsplash.com/photos/RKsLQoSnuTc'
+    url:'https://images.unsplash.com/photo-1514890547357-a9ee288728e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
     },
     {title: 'firenze',
-    url:'https://unsplash.com/photos/Qq1fwSLM0N0'
+    url:'https://images.unsplash.com/photo-1528114039593-4366cc08227d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     }
-  ]
+  ];
+
 }
