088ed13a0cf19822e6af149b5cb39ee9d3b6df8d
GillianY1 <gilliangilliany@gmail.com>
2022-10-11 15:48:29 +0800

    add custom pipes

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -10,8 +10,18 @@ export class AppComponent {
  payDate: string | undefined;
  amount: number | undefined;
  height: number | undefined;
+ miles: number | undefined;
 
- onNameChange(event: Event){
+ car = {
+  make: 'Toyota',
+  modal : 'Camry',
+  year: 2000
+ };
+ onMilesChange(event: Event){
+  this.miles= parseFloat((event.target as HTMLInputElement).value);
+ }
+
+ onNameChange(event: Event): void{
   this.name = (event.target as HTMLInputElement).value;
  }
  onDateChange(event: Event){
